1059fe182c30d95ff619c73b20e6bae0c4108b14
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-21 19:51:38 +0530

    Add project files.

---
+++ No_Static method hidding/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace No_Static_method_hidding
+{
+    class MyClass
+    {
+        public  void name()
+        {
+            Console.WriteLine("Hello :");
+        }
+        public void show()
+        {
+            name();
+            Console.WriteLine("No static methid :");
+        }
+    }
+    class Program:MyClass
+    {
+        public  new void show()
+        {
+            base.show();
+            
+            Console.WriteLine("No Static Method Hidding :");
+        }
+        static void Main(string[] args)
+        {
+           MyClass obj = new Program();
+            obj.show();
+        }
+    }
+}
